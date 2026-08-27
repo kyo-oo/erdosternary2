64cafe98aa0f423da7e981277f016e8995398be5
kyo-oo <lordoftreasures7@gmail.com>
2026-08-27 10:46:47 +0530

    Probe canonical tail primitives

---
+++ GSTCanonicalTailSymbolProbe.lean
@@ -0,0 +1,20 @@
+import GSTNavigationCore
+
+set_option maxRecDepth 1000000
+set_option maxHeartbeats 10000000
+
+#check gst_prefixed_tail_digitS
+#check gst_prefixed_tail_carryS
+#check gst_seeded_affine_digit_shiftS
+#check gst_seeded_affine_carry_semigroupS
+#check gstCarryS
+#check gstDigitS
+#check gstAffineMulCarryS
+#check gst_navigation_core_decompositionS
+#check GSTGraphV2HandwrittenExponentialLTE.pow4_scaled_mod_next
+#check Nat.div_div_eq_div_mul
+#check Nat.mul_add_mod
+#check Nat.add_mul_mod_self_left
+#check Nat.add_mul_div_left
+#check Nat.mul_add_div
+#check Nat.div_eq_of_lt_le
