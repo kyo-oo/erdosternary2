a3bec91f930f10f49a6f6d9c3e79e429c4d1ad6a
kyo-oo <lordoftreasures7@gmail.com>
2026-08-30 10:47:22 +0530

    Use full Graph V2 descent ontology in collision TDD

--- GSTGraphV2PerfectPowerBlockCollision.lean
+++ GSTGraphV2PerfectPowerBlockCollision.lean
@@ -1,6 +1,6 @@
 import GSTGraphV2PerfectPowerBlockProbe
 import GSTU2DPureDivergence83
-import GSTGraphV2Ontological
+import GSTGraphV2DescentOntology
 
 set_option maxRecDepth 1000000
 set_option maxHeartbeats 10000000
