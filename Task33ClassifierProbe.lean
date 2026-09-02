c59670815993a319588d7e9bf687990683a6e164
kyo-oo <lordoftreasures7@gmail.com>
2026-09-02 08:13:13 +0530

    Probe Task33 residual classifier types

---
+++ Task33ClassifierProbe.lean
@@ -0,0 +1,11 @@
+import GSTFinalResidualConnector
+
+set_option pp.universes false
+set_option pp.all false
+
+#print GSTOmegaInfiniteBadTrace
+#print GSTNavigationWitness
+#print gstNavigationConstant
+#print gstOmega
+#print gst_omega_parent_projection
+#print gst_omega_gate_polynomial_zero_iff
