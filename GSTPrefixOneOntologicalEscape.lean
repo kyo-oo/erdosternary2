b1390d7b0a6e488fa54a0920eab18c84d44bad74
kyo-oo <lordoftreasures7@gmail.com>
2026-09-04 08:20:05 +0530

    Expose four-power creation certificate to monolith

--- GSTPrefixOneOntologicalEscape.lean
+++ GSTPrefixOneOntologicalEscape.lean
@@ -1,5 +1,9 @@
 import GSTPerfectPowerTailNavigation
 import GSTFourPowerOntologicalAdapter
+import GSTFourPowerDirectCreationMaster
+import GSTFourPowerDirectHappyBridge
+import GSTFourPowerDirectFailedRelocationState
+import GSTInfiniteFourPowerNavigation
 
 set_option maxRecDepth 1000000
 set_option maxHeartbeats 10000000
