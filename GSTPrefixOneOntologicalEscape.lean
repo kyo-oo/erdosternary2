efd974fa94ead7170876ffc0a725a024a7daba1d
kyo-oo <lordoftreasures7@gmail.com>
2026-09-04 12:33:03 +0530

    Remove stale infinite four-power import

--- GSTPrefixOneOntologicalEscape.lean
+++ GSTPrefixOneOntologicalEscape.lean
@@ -3,7 +3,6 @@ import GSTFourPowerOntologicalAdapter
 import GSTFourPowerDirectCreationMaster
 import GSTFourPowerDirectHappyBridge
 import GSTFourPowerDirectFailedRelocationState
-import GSTInfiniteFourPowerNavigation
 
 set_option maxRecDepth 1000000
 set_option maxHeartbeats 10000000
